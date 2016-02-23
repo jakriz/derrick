9b4f0965885b681af641679a3510d9454aa77d9d
Jakub Kriz <jacob.kriz@gmail.com>
2016-02-24 00:20:45 +0100

    Removed signing from default assemble

--- core/src/main/java/io/github/jakriz/derrick/Derrick.java
+++ core/src/main/java/io/github/jakriz/derrick/Derrick.java
@@ -4,12 +4,12 @@ import io.github.jakriz.derrick.processor.Namer;
 
 /**
  * The entry point to the library. Used to get the
- * implemented instances of the defined @DerrickInterface interfaces.
+ * implemented instances of the defined {@code @DerrickInterface} interfaces.
  */
 public class Derrick {
 
     /**
-     * Returns an instance of {@param klass} interface implemented by Derrick.
+     * Returns an instance of {@code klass} interface implemented by Derrick.
      */
     public static <T> T get(Class<T> klass) {
         try {
